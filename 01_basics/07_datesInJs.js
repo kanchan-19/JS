14ec165e4aa3b90ce8b973a8bd80cba09b647ee0
kanchan-19 <66782083+kanchan-19@users.noreply.github.com>
2025-02-24 21:06:35 +0000

    Array part 1

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -7,9 +7,26 @@ console.log(myDate.toLocaleString()); // o/p: 2/24/2025, 1:26:21 PM
 console.log(myDate.toLocaleDateString()); // o/p: 2/24/2025
 console.log(typeof(myDate)); // o/p: object
 
-let myCreatedDate = new Date(2025, 0, 23); // month start with 0 in JS
+let myCreatedDate = new Date(2025, 0, 23); // month start with 0 in JS when we write in comma format
 console.log(myCreatedDate.toString()); // o/p: Thu Jan 23 2025 00:00:00 GMT+0000 (Coordinated Universal Time)
 
 myCreatedDate = new Date(2025, 0, 23, 5, 3);
 console.log(myCreatedDate.toLocaleString()); // o/p: 1/23/2025, 5:03:00 AM
+
+myCreatedDate = new Date("2025-2-25");
+console.log(myCreatedDate.toLocaleString()); // o/p: 2/25/2025, 12:00:00 AM
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp); // o/p: 1740428703857 => time is in ms
+console.log(myCreatedDate.getTime()); // o/p: 1740441600000 => time is in ms
+console.log(Math.floor(Date.now()/1000)); // o/p: 1740428703 => time is divided by 1000 to convert it in second
+
+let newDate = new Date();
+console.log(newDate); // o/p: 2025-02-24T20:29:18.829Z
+console.log(newDate.getMonth()); // o/p: 1 => month starts from 0
+console.log(newDate.getDay()); // o/p: 1
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+})); // o/p: Monday => Here default is format
 
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,34 @@
+// Arrays in JavaScript is resizable and it can contain mixed data types unlike java
+
+const myArray = [0, 1, 2, 3, 4,  5];
+console.log(myArray[0]);
+const myHeros = ["Shaktiman", "Krishna", "Naagraj"];
+const myArr2 = new Array(1, 2, 3, 4);
+
+//Array Methods
+
+myArray.push(6);
+myArray.push(7);
+myArray.pop(); // deletes value from the end of the array
+myArray.unshift(9); // adds value at the start of the array
+console.log(myArray);
+myArray.shift(); // delets value from the start of the array
+console.log(myArray);
+console.log(myArray.includes(9));
+console.log(myArray.indexOf(9)); // o/p: -1 => this means that this value is not present in array
+
+const newArr = myArray.join();
+
+console.log(newArr); // o/p: 0,1,2,3,4,5,6 => join function converts the array in string
+
+//slice and splice
+
+console.log("A", myArray); // o/p: A [ 0, 1, 2, 3, 4, 5, 6 ]
+
+const myn1 = myArray.slice(1, 3);
+console.log(myn1); // o/p: [ 1, 2 ]
+console.log("B", myArray); // o/p: B [0, 1, 2, 3, 4, 5, 6 ]
+
+const myn2 = myArray.splice(1, 3); // splice function changes the original array and and removes that portion portion of values from the array
+console.log("C", myArray); // o/p: C [ 0, 4, 5, 6 ]
+console.log(myn2); // o/p: [ 1, 2, 3 ]
