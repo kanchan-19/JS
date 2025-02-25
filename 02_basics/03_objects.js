1d06287af1c419c300000840b40cbd8c9216999f
kanchan-19 <66782083+kanchan-19@users.noreply.github.com>
2025-02-25 21:00:15 +0000

    object part 3

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -44,3 +44,78 @@ JSUser.greetingTwo = function(){
 console.log(JSUser.greeting()); // o/p: Hello JS USer
 console.log(JSUser.greetingTwo()); // o/p: Hello! JS user, Kanchan
 
+/********************* Creating singleton object *****************************/
+
+const tinderUser = new Object();
+
+console.log(tinderUser); // o/p: {}
+
+tinderUser.id = "123fgh";
+tinderUser.name = "Sam";
+tinderUser.isLoggedIn = false;
+console.log(tinderUser); // o/p: { id: '123fgh', name: 'Sam', isLoggedIn: false }
+
+/********************************************************************************************/
+const regularUser = {
+    email: "abc@amazon.com",
+    fullName: {
+        userFullName: {
+            firstName: "Kanchan",
+            lastName: "Kumari",
+        },
+    },
+};
+
+console.log(regularUser.fullName.userFullName); // o/p: { firstName: 'Kanchan', lastName: 'Kumari' }
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "a", 4: "b"};
+const obj3 = {5: "a", 6: "b"};
+
+const obj4 = {obj1, obj2, obj3};
+console.log(obj4); /* {
+    obj1: { '1': 'a', '2': 'b' },
+    obj2: { '3': 'a', '4': 'b' },
+    obj3: { '5': 'a', '6': 'b' }
+  }
+    */
+
+const obj5 = Object.assign({}, obj1, obj2, obj3); // this means we are concatenating the key value pair of all objects (obj1, obj2, obj3) and assigning it to empty object
+console.log(obj5); // o/p: { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const obj6 = {...obj1, ...obj2, ...obj3};
+console.log(obj6); // o/p: { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com",
+    },
+    {
+        id: 2,
+        email: "abc@orkut.com",
+    }
+];
+
+console.log(users[1].email); // o/p: abc@orkut.com
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser)); // o/p: [ 'id', 'name', 'isLoggedIn' ] => gives all keys of an object in the form of array
+console.log(Object.values(tinderUser)); // o/p: [ '123fgh', 'Sam', false ] => gives all values of an object in array form
+console.log(Object.entries(tinderUser)); // o/p: [ [ 'id', '123fgh' ], [ 'name', 'Sam' ], [ 'isLoggedIn', false ] ] => gives key value pairs in the form of array
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // o/p: true => used to check if that key exists in object or not
+
+/************************** Object De-structuring **************************/
+
+const course = {
+    courseName: "JS",
+    price: 999,
+    courseInstructor: "Kanchan",
+};
+
+const {courseInstructor} = course;
+
+console.log(courseInstructor); // o/p: Kanchan
+
+const {courseInstructor: instructor} = course; // o/p: We can rename key as well to make it shorter or longer
+console.log(instructor); // o/p: Kanchan
