ff04161a3525afb6b838e9f5755d032c584c9f68
kanchan-19 <66782083+kanchan-19@users.noreply.github.com>
2025-03-21 02:47:29 +0000

    DOM

--- 06_dom/one.js
+++ 06_dom/one.js
@@ -1,1 +1,16 @@
-console.dir(document); // It doesn't work in node environment but it works on browser. In browser, this is used to get all the properties and methods of document
+// window is a complex objecy
+
+// console.dir(document); // It doesn't work in node environment but it works on browser. In browser, this is used to get all the properties and methods of document
+
+// HTML collection, node collection and array are three different things.
+
+/*
+console.log(document.links);
+console.log(document.links[2]);
+document.getElementById("firstHeading");
+document.getElementById("firstHeading").innerHTML = "<h1>Kanchan Kumari</h1>";
+*/
+
+// above cmds will work on console not in node environment
+
+
